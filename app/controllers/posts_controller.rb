a9c1f17a6ce04e2c5d93651652fc0606a17af077
bnb <deathproooof@gmail.com>
2023-06-09 12:07:00 +0900

    全体的に整形

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,8 +25,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+        format.html { redirect_to root_path}
+        format.json { render :index, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -42,11 +42,9 @@ class PostsController < ApplicationController
       @post.like+=1
     end
 
-    
-     
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: "Post was successfully created." }
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
         format.json { render :index, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -60,8 +58,8 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to posts_path, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
+        format.html { redirect_to root_path, notice: "Post was successfully updated." }
+        format.json { render :index, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  post "posts/:id/like" => 'posts#like'
- 
+  post '/posts/:id/like', to: 'posts#like', as: 'like_post'
+
   
   
   
