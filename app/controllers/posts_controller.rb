d93ea6bcf3b967b3d4c0941c120975401ca8949c
bnb <deathproooof@gmail.com>
2023-06-09 13:01:04 +0900

    s3と連携して画像アップ可能にした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -85,6 +85,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag)
+      params.require(:post).permit(:name, :content, :tag, :image)
     end
 end
